5e1d12c9740720ba9d637c296340072aa4c26e45
Emmanuel.Ayeh <Emmanuel.Ayeh@abujaelectricity.com>
2024-12-30 02:19:31 -1200

    Update 2024_12_28_211022_create_payments_table.php

--- database/migrations/2024_12_28_211022_create_payments_table.php
+++ database/migrations/2024_12_28_211022_create_payments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('subscription_id')->constrained()->onDelete('cascade');
+            $table->foreignId('plan_id')->constrained()->onDelete('cascade');
             $table->decimal('amount', 10, 2);
             $table->string('currency')->default('USD');
             $table->string('transaction_reference')->unique();
