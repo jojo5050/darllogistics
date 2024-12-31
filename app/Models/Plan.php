d970f952713a135f76335f47b77438faa86f0e05
Emmanuel.Ayeh <Emmanuel.Ayeh@abujaelectricity.com>
2024-12-30 21:10:17 -1200

    Update Plan.php

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'features',
+        'amount',
+        'discount',
+        'coupon'
+    ];
 }
