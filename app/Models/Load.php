de48e4a26755e42eb7cb99294165304a09f01ece
Emmanuel.Ayeh <=>
2025-02-12 08:38:10 -1200

    Update load model

--- app/Models/Load.php
+++ app/Models/Load.php
@@ -7,10 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Load extends Model
 {
     protected $fillable = [
-        'description',
-        'pickup_date',
-        'delivery_date',
-        'weight',
+        'user_id',
         'pickup_state',
         'pickup_time_range',
         'pickup_address',
@@ -20,7 +17,7 @@ class Load extends Model
         'commodities',
         'rate',
         'driver_id',
-        'assigned_vehicle_id',
+        'vehicle_id',
         'dispatcher_id',
         'fee_type',
         'amount'
