a9d6ee799f3ce91695b34294a1cc25834d49f8c5
Emmanuel.Ayeh <=>
2025-04-12 00:32:42 -1200

    Fix bugs

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'ceo',
+        'tel',
+        'mobile',
+        'country',
+        'state',
+        'city',
+        'zip_code',
+        'address',
+        'po_box',
+        'logo',
+        'email',
+        'email2',
+        'whatsapp',
+        'instagram',
+        'twitter',
+        'linkedin',
+        'facebook',
+        'website',
+    ];
 }
